d09ef970243267c58d3ad66f139a0d3f5d8b2628
dahua777 <1220860737@qq.com>
2015-11-11 21:30:32 +0800

    pointer
    指针第一次提交

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  pointer
+//
+//  Created by 陆天明 on 15/11/11.
+//  Copyright © 2015年 陆天明. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a,*p;
+    a=100;
+    p=&a;
+    
+    printf("a=%d  &a=%d  &p=%d  p=%d   *p=%d/n",a,&a,&p,p,*p);
+    return 0;
+}
